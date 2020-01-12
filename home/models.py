f1e66505dba649e35cab5d37934cf2009ecfaaa1
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2020-01-12 21:02:23 +0300

    Added dependencies

--- home/models.py
+++ home/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
 from wagtail.core.models import Page
 
 
 class HomePage(Page):
-    
-    lead_text = models.CharField(max_length=140, blank=True, help_text='Subheading title under the banner title')
-    
+
+    lead_text = models.CharField(
+        max_length=140, blank=True, help_text="Subheading title under the banner title"
+    )
--- mountaintop/settings/dev.py
+++ mountaintop/settings/dev.py
@@ -4,12 +4,12 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&4^s-na*^%r1y549b(#$%d0s&=#q!#0n#qde)!dp^^0io24yla'
+SECRET_KEY = "&4^s-na*^%r1y549b(#$%d0s&=#q!#0n#qde)!dp^^0io24yla"
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ["*"]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
 try:
