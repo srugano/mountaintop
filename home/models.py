ad9f4f22769da111e38014b8dd7b66adf9c8b5d6
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2020-01-14 11:38:58 +0300

    modified homepage

--- home/models.py
+++ home/models.py
@@ -1,9 +1,36 @@
 from django.db import models
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
 from wagtail.core.models import Page
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 
 class HomePage(Page):
 
     lead_text = models.CharField(
-        max_length=140, blank=True, help_text="Subheading title under the banner title"
+        max_length=140, blank=True, help_text="Subheading text under the banner title"
     )
+    button = models.ForeignKey(
+        "wagtailcore.Page",
+        blank=True,
+        null=True,
+        related_name="+",
+        help_text="Select an optional text to link to",
+        on_delete=models.SET_NULL,
+    )
+    button_text = models.CharField(
+        max_length=50, default="Read More", blank=False, help_text="Read mor text"
+    )
+    banner_background_image = models.ForeignKey(
+        "wagtailimages.Image",
+        blank=True,
+        null=True,
+        related_name="+",
+        help_text="The banner background image",
+        on_delete=models.SET_NULL,
+    )
+    content_panels = Page.content_panels + [
+        FieldPanel("lead_text"),
+        PageChooserPanel("button"),
+        FieldPanel("button_text"),
+        ImageChooserPanel("banner_background_image"),
+    ]
